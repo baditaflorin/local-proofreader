b9458a9b1ce576c904bcb22e46efa35fd0632d4c
Florin Badita-Nistor <baditaflorin@gmail.com>
2026-05-08 11:45:57 +0300

    ops: publish proofreader pages build

---
+++ docs/assets/duckdb-browser-mvp.worker-WReLCsRQ.js
@@ -0,0 +1,2 @@
+var e=`/local-proofreader/assets/duckdb-browser-mvp.worker-AS4FIczp.js`;export{e as default};
+//# sourceMappingURL=duckdb-browser-mvp.worker-WReLCsRQ.js.map
---
+++ docs/assets/duckdb-mvp-CMyIff11.js
@@ -0,0 +1,2 @@
+var e=`/local-proofreader/assets/duckdb-mvp-CNulJfH-.wasm`;export{e as default};
+//# sourceMappingURL=duckdb-mvp-CMyIff11.js.map
